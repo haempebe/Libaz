a4024a3e3e445178bfdb4b4b75e0e21e751a1c80
bazma-pdp <pdpsmktibazma@outlook.com>
2023-10-24 12:57:18 +0700

    category

--- libaz/resources/views/pages/category.blade.php
+++ libaz/resources/views/pages/category.blade.php
@@ -12,7 +12,7 @@
                             <table class="table align-items-center mb-0">
                                 <thead>
                                     <tr>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
                                             id</th>
                                         <th
                                             class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
@@ -28,10 +28,10 @@
                                 <tbody>
                                     <tr>
                                         <td>
-                                            <p class="text-xs font-weight-bold mb-0">1</p>
+                                            <p class="ps-3 text-xs font-weight-bold mb-0">1</p>
                                         </td>
                                         <td class="align-middle text-center text-sm">
-                                            <span class="badge badge-sm bg-gradient-success">Online</span>
+                                            <span class="text-secondary text-xs font-weight-bold">Religious</span>
                                         </td>
                                         <td class="align-middle text-center">
                                             <span class="text-secondary text-xs font-weight-bold">23/04/18</span>
@@ -39,12 +39,12 @@
                                         <td class="align-middle text-center">
                                             <a href="javascript:;"
                                                 class="btn btn-xs bg-gradient-success mb-0 font-weight-bold text-xs">
-                                                <span class="btn-inner--icon"><i class="ni ni-button-play"></i></span>
+                                                <span class="btn-inner--icon"><i class="fas fa-edit"></i></span>
                                                 <span class="btn-inner--text"> Edit</span>
                                             </a>
                                             <a href="javascript:;"
                                                 class="btn btn-xs bg-gradient-danger mb-0 font-weight-bold text-xs">
-                                                <span class="btn-inner--icon"><i class="ni ni-button-play"></i></span>
+                                                <span class="btn-inner--icon"><i class="fas fa-trash-alt"></i></span>
                                                 <span class="btn-inner--text"> Delete</span>
                                             </a>
                                         </td>
@@ -57,45 +57,24 @@
             </div>
             <div class="col-md-4">
                 <div class="card">
-                    <form role="form" method="POST" action="http://127.0.0.1:8000/profile"
+                    <form role="form" method="POST" action=""
                         enctype="multipart/form-data">
                         <input type="hidden" name="_token" value="CYtRh5L5EHvlIDQqqeovjLZXy8EYCCdirewrEOiV"
                             autocomplete="off">
                         <div class="card-header border-radius-lg pb-0">
-                            <p class="mb-0">+ New Category +</p>
+                            <p class="mb-0">+ New Category</p>
                         </div>
                         <div class="card-body">
-                            <p class="text-uppercase text-sm">Contact Information</p>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Address</label>
-                                        <input class="form-control" type="text" name="address" value=""
-                                            onfocus="focused(this)" onfocusout="defocused(this)">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">City</label>
-                                        <input class="form-control" type="text" name="city" value=""
-                                            onfocus="focused(this)" onfocusout="defocused(this)">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Country</label>
-                                        <input class="form-control" type="text" name="country" value=""
-                                            onfocus="focused(this)" onfocusout="defocused(this)">
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="example-text-input" class="form-control-label">Postal code</label>
-                                        <input class="form-control" type="text" name="postal" value=""
-                                            onfocus="focused(this)" onfocusout="defocused(this)">
-                                    </div>
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="example-text-input" class="form-control-label">Name</label>
+                                    <input class="form-control" type="text" name="name" value=""
+                                        onfocus="focused(this)" onfocusout="defocused(this)">
                                 </div>
                             </div>
+                        </div>
+                        <div class="card-footer">
+
                         </div>
                     </form>
                 </div>
