ff6945d0b32245233476b908208c964a317bf0c1
HFZALFTRH <hafizalfitrah200@gmail.com>
2023-12-01 07:50:47 +0700

    update menu

--- libaz/resources/views/users/about.blade.php
+++ libaz/resources/views/users/about.blade.php
@@ -3,73 +3,87 @@
 
 @section('content')
 
-  
+
     <div class="container text-center">
-        <div class="row align-items-center border-bottom py-5">
-            <div class="col-5 h2 text-start">tentang perpustakaan
-                <img src="./img/illustrations/image/logo-ct-dark.png" alt="logo libaz" class="w-70 mt-5">
+        <div class="row align-items-center py-5">
+            <div class="col-6 h2 text-start">tentang perpustakaan</div>
+            <div class="row align-items-center py-5">
+                <img src="./img/illustrations/image/logo-ct-dark.png" alt="logo libaz" class="w-35 mt-5 col-6 ms-3">
+
+
+                <div class="col-6 mt-5 ms-5" style="text-align: left ">
+                    LiBaz adalah platform Perpustakaan Digital SMK TI BAZMA, memadukan literasi dengan teknologi modern.
+                    Disini
+                    siswa dapat menjelajahi koleksi buku yang beragam, menjadikan proses pembelajaran lebih dinamis dan
+                    terkini.
+                    Untuk meningkatkan aksesibilitas, kami menyediakan media peminjaman buku digital, memudahkan siswa untuk
+                    mengakses dan menikmati bahan bacaan kapan saja.Berbagai rekomendasi buku-buku yang relevan dan menarik
+                    disesuaikan dengan minat juga kebutuhan belajar masing-masing Peserta Didik sehingga dapat meningkatkan
+                    minat literasi. LiBaz bukan hanya tempat untuk membaca, tetapi juga untuk memperluas wawasan dan
+                    mendukung
+                    pembelajaran. Selamat mengeksplorasi dan menikmati keberagaman pengetahuan di Perpustakaan Digital kami!
+                </div>
             </div>
-            <div class="col-6 mt-7 px-7 text-break  ">
-                LiBaz adalah platform Perpustakaan Digital SMK TI BAZMA, memadukan literasi dengan teknologi modern. Disini siswa dapat menjelajahi koleksi buku yang beragam, menjadikan proses pembelajaran lebih dinamis dan terkini. Untuk meningkatkan aksesibilitas, kami menyediakan media peminjaman buku digital, memudahkan siswa untuk mengakses dan menikmati bahan bacaan kapan saja.Berbagai rekomendasi buku-buku yang relevan dan menarik disesuaikan dengan minat juga kebutuhan belajar masing-masing Peserta Didik sehingga dapat meningkatkan minat literasi. LiBaz bukan hanya tempat untuk membaca, tetapi juga untuk memperluas wawasan dan mendukung pembelajaran. Selamat mengeksplorasi dan menikmati keberagaman pengetahuan di Perpustakaan Digital kami!
-            </div><div class="col-5 h2 text-start">libaz hadir sebagai solusi</div>
+            <div class="col-5 h2 text-start">libaz hadir sebagai solusi</div>
             <div class="border border-dark border-3 opacity-50 mt-7"></div>
         </div>
         <div class="col-6 h3" style="text-align: left;">informasi perpustakaan:</div>
-        <div class="col-3 h5 ms-5" style="text-align: left;">jam buka</div>
-        <div class="col-3" style="text-align: left;">
-            Senin-Jumat :
-            Buka : 08.00
-            istirahat : 12.00-13.00
-            Tutup : 15.00
-            Sabtu-minggu:
-            Buka : 10.00
-            istirahat : 12.00-13.00
-            Tutup : 15.00
+        <div class="row text start">
+            <div class="col-3 text-start sm-12">
+                <h5>Jam Buka</h5>
+                <p> Senin-Jumat :</p>
+                <p>Buka : 08.00</p>
+                <p>istirahat : 12.00-13.00</p>
+                <p>Tutup : 15.00</p>
+                <p>Sabtu-minggu:</p>
+                <p>Buka : 10.00</p>
+                <p>istirahat: 12.00-13.00</p>
+                <p>Tutup : 15.00</p>
+            </div>
+            <div class="col-12 text-start sm-12">
+                <h5>Peraturan Perpustakaan</h5>
+                <h6>Berikut aturan umum terkait dengan peminjaman buku di LIBAZ:</h6>
+                <p>1. Peminjam adalah anggota aktif LIBAZ;
+                </p>
+                <p>2. Maksimal durasi peminjaman buku adalah 7 hari, jika belum
+                    selesai maka wajib lapor;</p>
+                <p> 3. Batas jumlah buku yang dapat dipinjam
+                    adalah 1 buku dalam satu kali peminjaman;</p>
+
+                <p>
+                    4. Perpanjangan waktu peminjaman dapat dilakukan melalui portal dengan maksimal 3hari;
+                </p>
+                <p>5. Anggota perpustakaan akan menerima notifikasi otomatis menjelang batas waktu peminjaman berakhir;</p>
+                <p>6. Denda keterlambatan pengembalian buku berlaku jika terlambat tanpa ada perpanjangan:</p>
+                <p>7. Denda dihitung perhari;</p>
+                <p>8. Anggota bertanggung jawab penuh atas buku yang dipinjam, jika terjadi kerusakan atau kehilangan maka
+                    dikenakan biaya perbaikan atau penggantian;</p>
+                <p>9. Anggota diharap kan untuk memberikan ulasan atau rating terhadap buku yang telah mereka pinjam. Hal
+                    ini
+                    dapat membantu anggota lain dalam memilih buku yang sesuai dengan minat dan kebutuhan mereka;</p>
+                <p>10. Dilarang meminjam menggunakan akun orang lain.</p>
+            </div>
         </div>
-        <div class="col-3 h5 my-3">peraturan perpustakaan</div>
-        <div class="row">
-            <div class="col-12" style="text-align: left;">1. warga sekolah selain warga sekolah dilarang meminjam buku</div>
-            <div class="col-12" style="text-align: left;">2. siswa dapat meminjam hanya 1 buku </div>
-            <div class="col-12" style="text-align: left;" style= font-size: 12px;>3. peminjaman harus dilalakukan online melalui website ini 
-                apabila ada kerusakan pada buku saat peminjaman segera lapor ke guru</div>
-            <div class="col-12" style="text-align: left;">4. tidak boleh melakukan peminjaman menggunakan akun orang lain</div>
 
-              
+        <div class="col-lg-12 h6" style="text-align: right;"><a href="#"><U>perjalanan singkat</U></a></div>
 
-        </div>
-        <div class="col-3 h5 ms-2 ps-1 mt-3">join membership</div>
-        <div class="col-lg-12" style="text-align: left;">klik link ini untuk join <a href="#"><U>klik disini</U></a></div>
-        
-        <div class="col-3 h5 ms-3 mt-3">contact information</div>
-        <div class="col-6" style="text-align: left;">
-            <div class="h6"> Alamat :</div>
-Jl. Raya Cikampak Cicadas, RT.1/RW.1, Cicadas, Kec. Ciampea, Kabupaten Bogor, Jawa Barat 16620
-<div class="h6 mt-3">No Tlp  :</div>
-<div class="col-6 " style="text-align: left;">
-    081212223232323232</div>
-    <div class="h6 mt-3">Email :</div>
-<div class="col-6 " style="text-align: left;">
-    libaz@smk.ti.bazma</div>
-    
-
+        <div class="row mx-5">
+            <div class="col-3 px-3">
+                <img class="d-block w-100 mt-5 rounded-3" src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg">
+            </div>
+            <div class="col-3 px-3">
+                <img class="d-block w-100 mt-5 rounded-3" src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg">
+            </div>
+            <div class="col-3 px-3">
+                <img class="d-block w-100 mt-5 rounded-3" src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg">
+            </div>
+            <div class="col-3 px-3">
+                <img class="d-block w-100 mt-5 rounded-3" src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg">
+            </div>
+            <div class="col-3 h4 mt-5 " style="text-align: left;">Image Tim</div>
+            <div class="col-12 px-5"> <img class="d-block w-60 mt-5" src="./img/illustrations/image/logo-ct-dark.png"
+                    style="item-align-center"></div>
         </div>
     </div>
-    <div class="col-lg-12 h6" style="text-align: right;"><a href="#"><U>perjalanan singkat</U></a></div>
-    
-    <div class="row mx-10">
-        <div class="col-3 px-3"> 
-            <img class="d-block w-100 mt-5 rounded-3"
-                src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg"></div>
-        <div class="col-3 px-3"> 
-            <img class="d-block w-100 mt-5 rounded-3"
-                src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg"></div>
-        <div class="col-3 px-3"> 
-            <img class="d-block w-100 mt-5 rounded-3"
-                src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg"></div>
-        <div class="col-3 px-3"> 
-            <img class="d-block w-100 mt-5 rounded-3"
-                src="https://www.bukukita.com/babacms/displaybuku/71047_f.jpg"></div>
-    <div class="col-2 h4 mt-5 " style="text-align: left;">Image Tim</div>
-    <div class="col-12 px-8"> <img class="d-block w-100 mt-5" src="https://unissula.ac.id/wp-content/uploads/2023/03/pak-pur.jpg"></div>
 
 @endsection
